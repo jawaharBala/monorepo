1bbf48990047d55b3b13923368aa8f4231c06890
Jawahar Balasubramani <jawaharbjkk@gmail.com>
2023-06-08 15:33:18 -0400

    added angular

--- apps/ecommerce-navbar/src/app/app.tsx
+++ apps/ecommerce-navbar/src/app/app.tsx
@@ -1,13 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <h1>E-commerce navbar</h1>
-      
+
     </div>
   );
 }
